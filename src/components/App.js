bba1a0d025fd18be9f2a205e9a9b6308fbbd7d66
Jory Roberts <robertsjory13@gmail.com>
2023-06-05 02:57:29 -0500

    completes lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,40 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+import { data } from '../mocks/data';
+
+const App = () => {
+  const [image, setImage] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setImage(data.message);
+        setIsLoaded(true);
+      })
+      .catch((error) => {
+        console.error('Error fetching the image:', error);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  return (
+    <div>
+      <p>
+        {isLoaded ? (
+          image ? (
+            <img
+              src={image}
+              alt='A Random Dog'
+            />
+          ) : (
+            'No image found'
+          )
+        ) : (
+          'Loading'
+        )}
+      </p>
+    </div>
+  );
+};
+export default App;
